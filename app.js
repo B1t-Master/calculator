a00c2ac58ae3b1c4b6b697b29742468f97c49d94
B1t-Master <isaac.njuguna@strathmore.edu~.noreply.github.com>
2024-09-05 08:58:52 +0300

    Add basic integration with interface
    Still has issues you can continue adding digits to the totalled value,other buttons dont work yet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let operand1, operand2 = 0;
-let operator;
+let display = document.querySelector(".screen");
+let btn = document.querySelectorAll("button");
+let btns = Array.from(btn);
 
 function add(operand1, operand2) {
     return operand1 + operand2;
@@ -36,30 +37,73 @@ function backspace() {
 
 
 const operate = function (operand1, operand2, operator) {
+    operand1 = +operand1;
+    operand2 = +operand2;
+    let calculation;
     switch (operator) {
-        case "+":
-            add(operand1, operand2);
+        case "plus":
+            calculation = add(operand1, operand2);
             break;
-        case "-":
-            subtract(operand1, operand2);
+        case "minus":
+            calculation = subtract(operand1, operand2);
             break;
-        case "×":
-            multiply(operand1, operand2);
+        case "multiplication":
+            calculation = multiply(operand1, operand2);
             break;
-        case "÷":
-            divide(operand1, operand2);
+        case "division":
+            calculation = divide(operand1, operand2);
             break;
-        case "C":
+        case "clear":
             clear();
             break;
-        case "-/+":
+        case "change_sign":
             changeSign();
             break;
-        case "⌦":
+        case "backspace":
             backspace();
             break;
         default:
             break;
     }
-
+    return calculation;
 }
+
+
+let operand1 = "";
+let operand2 = '';
+let operator;
+let inputLoop = false;
+let operatorPressed = false;
+
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', (e) => {
+
+        if (btn[i].className === "numeric" && (operatorPressed == false)) {
+            display.textContent += `${btn[i].value}`;
+            operand1 += btns[i].value;
+        }
+
+        else if (btn[i].className === "operator" && (operatorPressed == false)) {
+            display.textContent = "";
+            operator = btns[i].value;
+            operatorPressed = true;
+        }
+
+        else if (btn[i].className === "numeric" && (operatorPressed == true)) {
+            display.textContent += `${btn[i].value}`;
+            operand2 += btns[i].value;
+        }
+
+        if (btn[i].value === "equals" && operatorPressed == true) {
+
+            display.textContent = operate(operand1, operand2, operator);
+            //display.textContent = `${operand1}`
+            operatorPressed = false;
+            //operand2 = "";
+        }
+    });
+}
+
+
+
